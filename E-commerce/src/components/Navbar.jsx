089c6cf06d429eef352861292f2997bc8caccf80
walter-wf <walter.franz@gmail.com>
2023-10-20 18:06:19 -0300

    correcciones

--- E-commerce/src/components/Navbar.jsx
+++ E-commerce/src/components/Navbar.jsx
@@ -1,28 +1,44 @@
-import 'bootstrap/dist/css/bootstrap.css';
+
+import "bootstrap/dist/css/bootstrap.css";
 import { Cartwidget } from "./Cartwidget";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="navbar-brand">ElectroLand</div>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/electronica">Electronica</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/computadoras">Computadoras</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/celulares">Celulares</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/consolas">Consolas</a>
-        </li>
-      </ul>
-      <Cartwidget />
+      <div className="container justify-content-center">
+        <a className="navbar-brand" href="/">
+          ElectroLand
+        </a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/electronica">
+              Electronica
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/computadoras">
+              Computadoras
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/celulares">
+              Celulares
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/consolas">
+              Consolas
+            </a>
+          </li>
+        </ul>
+        <Cartwidget />
+      </div>
     </nav>
   );
 };
+
